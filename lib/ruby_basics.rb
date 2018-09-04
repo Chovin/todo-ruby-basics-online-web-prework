a71e78e9c104836b079f2d27086b0041e3fa03f5
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-04 04:13:37 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,13 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg)
+  arg  
 end
 
 def greeting
@@ -15,13 +15,13 @@ def greeting
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
